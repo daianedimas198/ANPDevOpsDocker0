79477a405c55c7fdbffdf8d5522071048bc5e9fb
Renato Groffe <renato.groffe@yahoo.com.br>
2020-05-23 11:39:41 -0300

    Update Index.cshtml.cs

--- SiteContagem/Pages/Index.cshtml.cs
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -21,7 +21,7 @@ namespace SiteContagem.Pages
                 TempData["Local"] = _CONTADOR.Local;
                 TempData["Kernel"] = _CONTADOR.Kernel;
                 TempData["TargetFramework"] = _CONTADOR.TargetFramework;
-                TempData["MensagemFixa"] = "Azure DevOps + Docker";
+                TempData["MensagemFixa"] = "Azure DevOps + Docker + Kubernetes";
                 TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
             }            
         }
